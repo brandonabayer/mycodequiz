3b11317f129b21c9489ceb1fe30456348aae38ac
Brandon Bayer <brandonabayer@gmail.com>
2020-01-21 18:28:19 -0600

    timer event onclick

--- questions.js
+++ questions.js
@@ -9,5 +9,19 @@ var questions = [
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
-    //etc...
+    {
+        title: "What does DOM in JavaScript stand for?",
+        choices: ["DOminican Republic", "Days On Market", "Data On Master Group", "Document Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "Which operator has the value of 'not equal'?",
+        choices: ["!=", "0=", "/=", "null="],
+        answer: "!="
+    },
+    {
+        title: "What is Bootstrap?",
+        choices: ["a shoe", "a free and open-source CSS framework", "a strap", "a stylesheet language"],
+        answer: "a free and open-source CSS framework"
+    },
 ];
